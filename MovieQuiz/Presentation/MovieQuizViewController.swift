bf21709312fea5f2f1b00e754dec0713c0029bce
resxton <gornomirov661@gmail.com>
2024-11-17 18:18:47 +0300

    Tactile response and disabling buttons

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -22,6 +22,8 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
     
     
     // MARK: - Lifecycle
@@ -56,32 +58,53 @@ final class MovieQuizViewController: UIViewController {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
+        enableButtons()
     }
     
     
     private func showAnswerResult(isCorrect: Bool) {
         imageView.layer.borderWidth = 8
-        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.masksToBounds = true
         
         if isCorrect == true {
             imageView.layer.borderColor = UIColor.ypGreen.cgColor
+            correctAnswerFeedback()
             correctAnswers += 1
         } else {
             imageView.layer.borderColor = UIColor.ypRed.cgColor
+            wrongAnswerFeedback()
         }
         
+        disableButtons()
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.hideResult()
             self.showNextQuestionOrResults()
         }
     }
     
     
+    private func hideResult() {
+        imageView.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    
+    private func enableButtons() {
+        yesButton.isEnabled = true
+        noButton.isEnabled = true
+    }
+    
+    
+    private func disableButtons() {
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+    }
+    
+    
     private func showNextQuestionOrResults() {
-        print(currentQuestionIndex, questions.count)
-        if currentQuestionIndex == questions.count - 1 { // 1
-            // идём в состояние "Результат квиза"
+        if currentQuestionIndex == questions.count - 1 {
             show(quiz: QuizResultsViewModel(title: "Этот раунд окончен!", text: "Ваш результат \(correctAnswers)/\(questions.count)", buttonText: "Начать заново"))
-        } else { // 2
+        } else {
             currentQuestionIndex += 1
             
             let nextQuestion = questions[currentQuestionIndex]
@@ -100,10 +123,8 @@ final class MovieQuizViewController: UIViewController {
 
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
-            // сбрасываем переменную с количеством правильных ответов
             self.correctAnswers = 0
             
-            // заново показываем первый вопрос
             let firstQuestion = self.questions[self.currentQuestionIndex]
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
@@ -113,101 +134,53 @@ final class MovieQuizViewController: UIViewController {
 
         self.present(alert, animated: true, completion: nil)
     }
+    
+    
+    private func correctAnswerFeedback() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+        
+        let impact = UIImpactFeedbackGenerator(style: .light)
+        impact.impactOccurred()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            impact.impactOccurred(intensity: 0.7)
+        }
+    }
+    
+    
+    private func wrongAnswerFeedback() {
+        let impact = UIImpactFeedbackGenerator(style: .medium)
+        impact.impactOccurred()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            impact.impactOccurred(intensity: 0.5)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            impact.impactOccurred(intensity: 1.0)
+        }
+    }
 }
 
 
+// MARK: - Models
 struct QuizQuestion {
-  // строка с названием фильма,
-  // совпадает с названием картинки афиши фильма в Assets
   let image: String
-  // строка с вопросом о рейтинге фильма
   let text: String
-  // булевое значение (true, false), правильный ответ на вопрос
   let correctAnswer: Bool
 }
 
 
 struct QuizStepViewModel {
-  // картинка с афишей фильма с типом UIImage
   let image: UIImage
-  // вопрос о рейтинге квиза
   let question: String
-  // строка с порядковым номером этого вопроса (ex. "1/10")
   let questionNumber: String
 }
 
 
-// для состояния "Результат квиза"
 struct QuizResultsViewModel {
-  // строка с заголовком алерта
   let title: String
-  // строка с текстом о количестве набранных очков
   let text: String
-  // текст для кнопки алерта
   let buttonText: String
 }
-
-// MARK: - Mock
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-*/
